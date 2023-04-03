f856a1544bc866b69b7a050fdd504675602fd951
Misha sysoiev <vsisoev@ukr.net>
2023-04-03 22:29:45 +0300

    f res2

--- main.js
+++ main.js
@@ -1,37 +1,73 @@
 let newUser = {
-  firstName:"",
-  lastName:"",
+  _firstName:"",
+  _lastName:"",
+  _birthday: '1995, 11, 17',
 
   getLogin: function(fName, lName){
     let sumString = fName[0] + lName;
+    this._firstName = fName;
+    this._lastName = lName;
     alert(sumString);
   }
 }
 
+Object.defineProperty(newUser, 'birthday',{
+  get(){
+    return this._birthday;
+  },
+  set(val){
+    this._birthday = val;
+  }
+});
+
+Object.defineProperty(newUser, 'firstName',{
+  get(){
+    return this._firstName;
+  },
+  set(val){
+    this._firstName = val;
+  }
+});
+
+Object.defineProperty(newUser, 'lastName',{
+  get(){
+    return this._lastName;
+  },
+  set(val){
+    this._lastName = val;
+  }
+});
 
 function createNewUser(){
+  let fName = prompt("f");
+  let lName = prompt("l");
   newUser.getLogin(fName, lName);
 
+  newUser._birthday = prompt("dd.mm.yyyy");
+
+  alert(getAge());
+  alert(getPassword());
+  
   return newUser;
 }
 
-function setFirstName(){
-  let fName = prompt("f");
+function getAge(){
+  let str = newUser._birthday;
+  let arr = str.split('.');
+  let year = arr[2];
 
-  newUser.firstName = fName;
+  let datess = 2023 - Number(year);
 
-  return fName;
+  return datess;
 }
 
-function setLastName(){
-  let lName = prompt("l");
+function getPassword(){
+  let strw = newUser._birthday;
+  let arr = strw.split('.');
+  let year = arr[2];
 
-  newUser.lastName = lName;
-
-  return lName;
+  let str = newUser._firstName[0].toUpperCase() + newUser._lastName.toLowerCase() + year;
+  return str;
 }
-
-let fName = setFirstName();
-let lName = setLastName();
 
 createNewUser();
